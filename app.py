b98a233ce5ede25f7723b671bf168d730a6247f8
poorva jadhav <poorva22jadhav@gmail.com>
2025-07-06 21:25:33 +0530

    Fixed duplicate endpoint function names

--- app.py
+++ app.py
@@ -2,16 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-posts = {
-    "first-post": {
-        "title": "The 'I' Paradox – First Post",
-        "content": "This is my first post exploring the mystery of self and existence."
-    },
-    "second-post": {
-        "title": "The 'I' Paradox – Second Post",
-        "content": "This is my second post diving deeper into self awareness."
-    }
-}
 
 
 @app.route('/')
@@ -26,13 +16,6 @@ def about():
 def blog():
     return render_template('blog.html')
 
-@app.route('/blog/<blog_name>')
-def blog(blog_name):
-    blog = blogs.get(blog_name)
-    if blog:
-        return render_template('blog.html', blog=blog)
-    else:
-        return "Blog not found", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
